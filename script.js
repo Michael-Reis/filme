3788d2287ea6219833ee1e929989e7ead99d99ef
Michael Reis <michael_farias14@hotmail.com>
2021-08-19 09:47:50 -0300

    layout modificação

--- script.js
+++ script.js
@@ -1,14 +1,56 @@
-    const API_KEY = 'bc2edd6da7c1980f09e65291361b7db1';
-    const BASE_URL = 'https://api.themoviedb.org/3'
-    const API_URL = BASE_URL + '/discover/movie/400?' + API_KEY;
-    console.log(API_URL); 
-
-
-    fetch(API_URL,{
-        method: "GET",  
-      }).then(function(resposta){
+(function(){
+    conteudodiv = document.querySelector('.filmes')
+    const fotofilme = "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/"
+    const filme = "star wars"
+        fetch("https://api.themoviedb.org/3/search/movie?api_key=bc2edd6da7c1980f09e65291361b7db1&language=pt-br&query="+ filme +"&page=1",{
+          method: "GET",  
+        }).then(function(resposta){
             resposta.json().then(function(respostaJson){
-            console.log(respostaJson)
+                console.log(respostaJson)
+                const quantidade = respostaJson.results.length
+                const listafilmes = respostaJson.results
+                
+                criandoitems = new Promise(
+                    (resolve, reject) => {
+                        listafilmes.forEach(filme => {
+                            if(filme.backdrop_path.length > 0){
+                                var divNova = document.createElement('div')
+                                var imagem = document.createElement('img')
+                                divNova.className = "grid-item"
+                                imagem.src = fotofilme + filme.backdrop_path 
+                                divNova.appendChild(imagem)
+                                conteudodiv.appendChild(divNova)                                   
+                                resolve();
+                            }
+                       
+                        });
+                    }
+                )
+                    
+                items = document.querySelectorAll('.grid-item')
+                for (let i = 0; i < items.length ; i++) {
+                
+                    var sombra = document.createElement('div')
+                    sombra.className = "sombra" 
+                    items[i].appendChild(sombra)
+                    
+                }
 
+                contador = 0;
+                listafilmes.forEach(filme => {
+                    if(filme.backdrop_path.length > 0){
+                        var titulo = document.createElement('h3')
+                        titulo.className = "original_title"
+                        titulo.innerText = filme.title
+                        items[contador].appendChild(titulo)
+                        contador++;
+                    }
+               
+                });
+
+
+    
+            })
         })
-    })
+
+})()
