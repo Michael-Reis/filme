b1770ade9692ebe109794295bde3b3ada29a0e93
Michael Reis <michael_farias14@hotmail.com>
2021-08-22 10:55:24 -0300

    alteracoes

---
+++ busca.js
@@ -0,0 +1,51 @@
+(function(){
+
+    var query = location.search.slice(1);
+    var partes = query.split('&');
+    var data = {};
+    partes.forEach(function (parte) {
+        var chaveValor = parte.split('=');
+        var chave = chaveValor[0];
+        var valor = chaveValor[1];
+        data[chave] = valor;
+    });
+
+    urlbasebanner ="https://www.themoviedb.org/t/p/w1920_and_h1080_bestv2/"; 
+    urlbaseposter ="https://www.themoviedb.org/t/p/w780/"; 
+    urlbaselogo ="https://themoviedb.org/t/p/w780/"; 
+
+    titulo = document.querySelector('.titulo')
+    sinopse = document.querySelector('.sinopse')
+    poster = document.querySelector('.poster')
+    banner = document.querySelector('.banner')
+    filmepesquisado = document.querySelector('#nomedofilme')
+    btnpesquisar = document.querySelector('#btnpesquisar')
+    logo = document.querySelector('.logo-filtrado')
+
+    api_key = "bc2edd6da7c1980f09e65291361b7db1";
+    pesquisaFilme(data.id)
+    function pesquisaFilme(idfilme){
+        fetch("https://api.themoviedb.org/3/movie/" + idfilme +"?api_key="+ api_key +"&language=pt-br",{method: "GET",  
+            }).then(function(resposta){resposta.json().then(function(respostaJson){
+                titulofilmeapi = respostaJson.title
+                titulofilme = titulofilmeapi.toUpperCase()
+                titulo.innerText = titulofilme
+                sinopse.innerText = respostaJson.overview
+                poster.src = urlbaseposter + respostaJson.poster_path
+                codigobanner = respostaJson.backdrop_path ? respostaJson.backdrop_path : respostaJson.poster_path
+                banner.src = urlbasebanner + codigobanner
+                logoapi = respostaJson.production_companies[0].logo_path
+                logo.src = urlbaselogo + logoapi
+                
+
+
+            })
+        })
+
+    }
+
+
+
+
+
+})()
--- script.js
+++ script.js
@@ -1,33 +1,51 @@
 (function(){
-    
+    filmepesquisado = document.querySelector('#nomedofilme')
+    btnpesquisar = document.querySelector('#btnpesquisar')
     conteudodiv = document.querySelector('.filmes')
     const fotofilme = "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/"
-    const filme = "star wars"
-        fetch("https://api.themoviedb.org/3/search/movie?api_key=bc2edd6da7c1980f09e65291361b7db1&language=pt-br&query="+ filme +"&page=1",{
-          method: "GET",  
-        }).then(function(resposta){
-            resposta.json().then(function(respostaJson){
+    const busca = "http://localhost/github/mydrugs/filme-api/busca.html?id="
+
+    btnpesquisar.addEventListener('click', function(){
+        filmesolicitado = filmepesquisado.value;  
+        lengthfilme = filmepesquisado.value.length
+        document.querySelectorAll('.grid-item').forEach((filmeanterior)=>{
+            filmeanterior.remove()
+        }) 
+        if(lengthfilme > 1){ 
+            pesquisaFilme(filmesolicitado) 
+        }else{
+            pesquisaFilme("star wars") 
+        }
+    })
+    
+    
+    function pesquisaFilme(nomedofilme){
+        fetch("https://api.themoviedb.org/3/search/movie?api_key=bc2edd6da7c1980f09e65291361b7db1&language=pt-br&query="+ nomedofilme +"&page=1",{method: "GET",  
+            }).then(function(resposta){
+                resposta.json().then(function(respostaJson){
                 console.log(respostaJson)
-                const quantidade = respostaJson.results.length
                 const listafilmes = respostaJson.results
-                
+                        
                 criandoitems = new Promise(
                     (resolve, reject) => {
                         listafilmes.forEach(filme => {
-                            if(filme.backdrop_path.length > 0){
+                            qtdfilmesdalista = filme.original_title.length
+                            var trataimagem =  filme.backdrop_path ?  /* filme.backdrop_path */  filme.poster_path :  filme.poster_path; 
+                            if(qtdfilmesdalista > 0){
                                 var divNova = document.createElement('div')
                                 var imagem = document.createElement('img')
                                 divNova.className = "grid-item"
-                                imagem.src = fotofilme + filme.backdrop_path 
+                                
+                                imagem.src = fotofilme + trataimagem
                                 divNova.appendChild(imagem)
                                 conteudodiv.appendChild(divNova)                                   
                                 resolve();
                             }
-                       
+                        
                         });
                     }
                 )
-                    
+                                
                 items = document.querySelectorAll('.grid-item')
                 for (let i = 0; i < items.length ; i++) {
                 
@@ -39,19 +57,45 @@
 
                 contador = 0;
                 listafilmes.forEach(filme => {
-                    if(filme.backdrop_path.length > 0){
+                    if(filme.original_title.length > 0){
                         var titulo = document.createElement('h3')
                         titulo.className = "original_title"
-                        titulo.innerText = filme.title
+                        titulo.innerText = filme.original_title
                         items[contador].appendChild(titulo)
                         contador++;
                     }
-               
+
                 });
 
+                contafilmes = 0;
+                listafilmes.forEach(filme => {
+                    if(filme.original_title.length > 0){
+                        linkdadosfilme = document.createElement('a')
+                        linkdadosfilme.className = "filmedirecionar"
+                        linkdadosfilme.href = busca + filme.id;
+                        items[contafilmes].appendChild(linkdadosfilme)
+                        contafilmes++;
+                    }
+                });
 
-    
+                sombras = document.querySelectorAll('.sombra')
+                linkfilme = document.querySelectorAll('.filmedirecionar')
+                for (let i = 0; i < linkfilme.length ; i++) {
+                    sombradados = sombras[i]
+                    linkfilme[i].appendChild(sombradados)
+                    
+                }
+
+
             })
         })
+
+    }
+
+    pesquisaFilme("star wars"); 
+
+        
+
+   
 
 })()
